27771dd6657fc10850d819732e7e23670e15fd2c
PLeenhouts <211506@student.fontys.nl>
2025-11-22 11:44:24 +0100

    Add files via upload

---
+++ src/data/artworks.js
@@ -0,0 +1,26 @@
+export const initialArtworks = [
+  {
+    id: "Kunstwerk 1",
+    title: "Kunstwerk 1",
+    artist: "Jan Onbekend",
+    year: 2025,
+    imageUrl: "https://picsum.photos/200/200",
+    description: "Een mooie foto 1",
+  },
+  {
+    id: "Kunstwerk 2",
+    title: "Kunstwerk 2",
+    artist: "Piet Onbekend",
+    year: 2024,
+    imageUrl: "https://picsum.photos/200/201",
+    description: "Een mooie foto 2",
+  },
+  {
+    id: "Kunstwerk 3",
+    title: "Kunstwerk 3",
+    artist: "Mieke Onbekend",
+    year: 2023,
+    imageUrl: "https://picsum.photos/200/202",
+    description: "Een mooie foto 3",
+  },
+];
