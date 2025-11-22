4ee60458672d5f8de73128525e36a19646f1c4da
PLeenhouts <211506@student.fontys.nl>
2025-11-22 11:43:26 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import './App.css'
+import { initialArtworks } from "./data/artworks.js";
 
 function App() {
+  const [artworks, setArtworks] = useState(initialArtworks);
   return (
     <div>
       <h1>Virtuele Kunstgalerij</h1>
-      <p>Hier komt straks onze React-app.</p>
+      <p>Een overzicht van kunstwerken</p>
+      <div className="art-grid">
+       {artworks.map((art) => (
+          <div className="art-card" key={art.id}>
+            <img src={art.imageUrl} alt={art.title}/>
+            <h2>{art.title}</h2>
+            <p>Artiest: {art.artist}</p>
+            <p>Jaar: {art.year}</p>
+            <p>Beschrijving: {art.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
+  
 export default App
